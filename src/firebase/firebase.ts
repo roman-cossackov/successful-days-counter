6564205cc8e2ba84384e29acad6ad39d36d03d94
roman-cossackov <roman.delarge@gmail.com>
2024-06-11 00:32:35 +0300

    fixes

--- src/firebase/firebase.ts
+++ src/firebase/firebase.ts
@@ -5,7 +5,6 @@ import { getFirestore } from "firebase/firestore";
 const firebaseConfig = {
   apiKey: import.meta.env.VITE_FIREBASE_APIKEY,
   authDomain: import.meta.env.VITE_FIREBASE_AUTHDOMAIN,
-  databaseURL: import.meta.env.VITE_FIREBASE_DATABASEURL,
   projectId: import.meta.env.VITE_FIREBASE_PROJECTID,
   storageBucket: import.meta.env.VITE_FIREBASE_STORAGEBUCKET,
   messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGINGSENDERID,
