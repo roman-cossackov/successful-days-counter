91dab0ae5529bbb4c12e44ede77e3d010aa7f348
roman-cossackov <roman.delarge@gmail.com>
2024-06-12 01:25:54 +0300

    fixes

--- src/firebase/firebase.ts
+++ src/firebase/firebase.ts
@@ -3,12 +3,12 @@ import { getAuth } from 'firebase/auth';
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: process.env.VITE_FIREBASE_APIKEY,
-  authDomain: process.env.VITE_FIREBASE_AUTHDOMAIN,
-  projectId: process.env.VITE_FIREBASE_PROJECTID,
-  storageBucket: process.env.VITE_FIREBASE_STORAGEBUCKET,
-  messagingSenderId: process.env.VITE_FIREBASE_MESSAGINGSENDERID,
-  appId: process.env.VITE_FIREBASE_APPID,
+  apiKey: import.meta.env.VITE_FIREBASE_APIKEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTHDOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECTID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGEBUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGINGSENDERID,
+  appId: import.meta.env.VITE_FIREBASE_APPID,
 };
 
 const app = initializeApp(firebaseConfig);
